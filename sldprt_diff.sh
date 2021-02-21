8c2635b1d3a89303685b1bea709b68a007f6c5bb
Ellie Clifford <git@clifford.lol>
2021-02-21 23:35:48 +0000

    Fix issues with diff and filenames

--- sldprt_diff.sh
+++ sldprt_diff.sh
@@ -1,20 +1,33 @@
 #!/bin/sh
-# executed by git: files to compare are $1 and $2
+# executed by git: I can't find actual documentation but I think
+# it is executed with the following args:
+# |  $1  |      $2      |  $3  |  $4  |      $5      |  $6  |  $7  |
+# | name | tmp_location | hash | mode | tmp_location | hash | mode |
+# |------|----------------------------|----------------------------|
+# |      |          left diff         |         right diff         |
 
 # TODO: some sort of macro thing
 # A macro can be invoked with e.g.
 #/c/Program\ Files/SOLIDWORKS\ Corp/SOLIDWORKS/SLDWORKS.exe /m sldworks-git-tools/compareprt.swp $1 $2
 
 # exit if the parts are identical
-if diff $1 $2 >/dev/null 2>/dev/null; then
-	exit 0
+if [ "$3" = "$6" ]; then
+	if ! [ "$4" = "$7" ]; then
+		echo "$1 differs in mode"
+		exit 1
+	else
+		exit 0
+	fi
 fi
 
-# move the second file somewhere nicer
-diff_filename="$(echo "$1" | sed 's/\(.*\)\.\([^.]*\)/\1_OTHER\.\2/')"
-cp $2 $diff_filename
+# move the files somewhere more understandable
+left_filename="$(echo "$1" | sed 's/\(.*\)\.\([^.]*\)/\1_LEFT\.\2/')"
+right_filename="$(echo "$1" | sed 's/\(.*\)\.\([^.]*\)/\1_RIGHT\.\2/')"
+cp $2 $left_filename
+cp $5 $right_filename
 
-echo "Parts $1 and $diff_filename differ."
+echo "Parts $left_filename and $right_filename differ."
 echo "You can use the compare feature in SolidWorks to see a diff."
-read -p "Press enter to continue (this will delete $diff_filename)"
-rm diff_filename
+echo -n "Press enter to continue "
+read -p "(this will delete $left_filename and $right_filename)"
+rm $left_filename $right_filename
